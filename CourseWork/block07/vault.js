d796113eee58c39ad32f24caad6890ef81012981
unknown <mrs.djana.ramos@gmail.com>
2024-12-02 16:34:30 -0600

    Coursework

---
+++ CourseWork/block07/vault.js
@@ -0,0 +1,12 @@
+//Create a html and js page
+//Link the pages together
+//Create a string for the user saying You have received this message because you have been chosen to open an important vault. Here is the secret combination: 121
+//Assign three variables
+//create three unique arithmetic operator
+//Add comments throughout the code to explain each step in the process
+//Create a dialog box displaying the vault codes and the text if using HTML and a script tag, or create a popup dialog
+
+alert(
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination: 1 2 1"
+);
+//This code gives the user an alert when coming to the page. It is a statement and ends with a semicolon
