f2f78fa050fe397418b0b4bf1b9dfd9086f07728
unknown <mrs.djana.ramos@gmail.com>
2024-12-03 20:15:45 -0600

    Adding Blocks

--- CourseWork/block07/vault.js
+++ CourseWork/block07/vault.js
@@ -10,3 +10,17 @@ alert(
   "You have received this message because you have been chosen to open an important vault. Here is the secret combination: 1 2 1"
 );
 //This code gives the user an alert when coming to the page. It is a statement and ends with a semicolon
+
+let myVariable = 1;
+console.log(myVariable);
+//the variable is declared 1
+
+let myV = [1, 2, 1];
+console.log(myV[1]);
+//This is an array and the output is 2 because, you start counting each spot starting with 0. So the 1 spot named in the console log would be the number 2 in the array
+
+let myVar = 1;
+if (myVar === 1) {
+  console.log(myVar);
+}
+//=== strictly equals one, making the if statement true and output 1
